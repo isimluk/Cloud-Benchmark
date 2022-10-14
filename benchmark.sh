a39cbd487e60809396804b1b752b82e91e98a933
Simon Lukasik <simon.lukasik@crowdstrike.com>
2022-10-14 14:01:58 +0100

    Better isolation during execution
    Make sure to operate in a single directory and not install pip packages for the
    user.

--- benchmark.sh
+++ benchmark.sh
@@ -2,48 +2,46 @@
 # Universal cloud provider provisioning calculator
 # Identifies the current cloud provider, then downloads the necessary scripts
 # to perform a sizing calculation.
-# 
+#
 # Creation date: 03.25.21, Joshua Hiller @ CrowdStrike
 #
 
-audit_AWS(){
-  # AWS audit
-  curl -o aws_count.py https://raw.githubusercontent.com/CrowdStrike/Cloud-Benchmark/main/AWS/aws_cspm_benchmark.py
-  pip3 install --user tabulate
-  python3 aws_count.py
-  rm aws_count.py
+base_url=https://raw.githubusercontent.com/isimluk/Cloud-Benchmark/test/
+
+audit(){
+    CLOUD="$1"
+    echo "This is ${CLOUD}"
+    cloud=$(echo "$CLOUD" | tr '[:upper:]' '[:lower:]')
+
+    curl -o requirements.txt "${base_url}/${CLOUD}/requirements.txt"
+    python3 -m pip install -r requirements.txt
+    file="${cloud}_cspm_benchmark.py"
+    curl -o "${file}" "${base_url}/${CLOUD}/${file}"
+    python3 "${file}"
 }
 
-audit_Azure(){
-  # Azure audit
-  curl -o azure_count.py https://raw.githubusercontent.com/CrowdStrike/Cloud-Benchmark/main/Azure/azure_cspm_benchmark.py
-  python3 -m pip install azure-mgmt-resource azure-mgmt-containerservice azure-identity msrestazure azure-mgmt-compute
-  python3 azure_count.py
-  rm azure_count.py
-}
-
-audit_GCP(){
-  # GCP audit
-  echo "NOT YET SUPPORTED"
-
-}
+python3 -m venv ./cloud-benchmark
+pushd ./cloud-benchmark
+source ./bin/activate
 
 # MAIN ROUTINE
 echo "Determining cloud provider"
 if type aws >/dev/null 2>&1; then
-  echo "This is AWS"
-  audit_AWS
+    audit "AWS"
 fi
-
 if type az >/dev/null 2>&1; then
-  echo "This is Azure"
-  audit_Azure
+    audit Azure
 fi
 
 if type gcloud >/dev/null 2>&1; then
-  echo "This is GCP"
-  audit_GCP
+    audit GCP
 fi
+
+popd
+deactivate
+
+echo "Type following command to export cloud counts:"
+echo "cat ./cloud-benchmark/*benchmark.csv"
 
 # END
 #
